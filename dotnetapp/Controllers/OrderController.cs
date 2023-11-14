96fafd17768d1c003b9453b15353ba73837699cb
Iamneo <techadmin@iamneo.ai>
2023-11-14 09:41:39 +0000

    Backup Commit

--- dotnetapp/Controllers/OrderController.cs
+++ dotnetapp/Controllers/OrderController.cs
@@ -4,10 +4,21 @@ using dotnetapp.Models;
 namespace dotnetapp.Controllers;
 public class OrderController:Controller
 {
-    private readonly AppDbContext context;
+  private readonly AppDbContext context;
   public OrderController(AppDbContext _context)
   {
     context=_context;
+  }
+  public IActionResult DisplayCustomers()
+  {
+    var data=context.Customers.ToList();
+    return View(data);
+  }
+
+  public IActionResult DisplayProductWithCat()
+  {
+    var data=from p in context.Products select new{p.ProductName,p.Category.CategoryName};
+    return View(data);
   }
 
 }
